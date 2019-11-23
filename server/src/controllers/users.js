5ebda0f4520a6d84490a84e926d16a95954ff5c5
Drakx116 <alexandre2.guyot@gmail.com>
2019-11-24 00:18:57 +0100

    Working UPDATE /users API Route

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -16,29 +16,44 @@ const addUser = (req ,res) => {
     let pseudo  = req.body.pseudo;
     let password = req.body.password;
 
+    if(!(pseudo && password)) {
+        res.status(400).send({
+            message: 'Invalid parameters.'
+        });
+    }
+
     newUser.save((error, user) => {
         if(error) {
             res.status(404).send(error);
         }
 
-        if(!(pseudo && password)) {
-            res.status(400).send('Invalid parameters.')
-        }
-
         // TODO : Encrypt password
 
-        res.status(201).send(user)
+        res.status(201).json(user)
+    });
+};
+
+const updateUser = (req, res) => {
+    User.updateOne({ _id : req.headers.userid}, req.body, (error, updatedUser) => {
+        if(error) {
+            res.send(error);
+        }
+
+        res.send({
+            'message': 'Updated User',
+            'user': updatedUser
+        })
     });
 };
 
 const deleteUser = (req, res) => {
     User.deleteOne({_id : req.body.userId }, (error) => {
         if(error) {
-            res.status(400).send(error);
+            res.send(error);
         }
 
-        res.json("User well deleted.")
+        res.send("User well deleted.")
     });
 };
 
-module.exports = { getAllUsers, addUser, deleteUser };
+module.exports = { getAllUsers, addUser, updateUser, deleteUser };
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -4,14 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 router
-    .get('/', (req, res) => {
-        users.getAllUsers(req, res);
-    })
-    .post('/', (req, res) => {
-        users.addUser(req, res);
-    })
-    .delete('/', (req, res) => {
-        users.deleteUser(req, res);
-    });
+    .get('/', (req, res) => users.getAllUsers(req, res))
+
+    .post('/', (req, res) => users.addUser(req, res))
+
+    .put('/', (req, res) => users.updateUser(req, res))
+
+    .delete('/', (req, res) => users.deleteUser(req, res));
 
 module.exports = router;
