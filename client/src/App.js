ac03d50fea402837dd83d910ab113777aa7b4484
Drakx116 <alexandre2.guyot@gmail.com>
2019-11-27 14:41:41 +0100

    Add React Router DOM

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import './App.css';
-import UserList from "./Components/UserList";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import UserList from "./Components/UserList/";
+import Dashboard from "./Components/Dashboard";
 
 class App extends React.Component {
   render() {
-    return (<UserList />)
+    return (
+      <Router>
+        <Switch>
+
+          <Route exact={true} path={"/"}>
+            <Dashboard />
+          </Route>
+
+          <Route path="/users">
+            <UserList />
+          </Route>
+
+        </Switch>
+      </Router>
+    );
   }
 }
 
